c15bd273ea1f6eb09b8e50da946d6ec376b5c406
Poulami Das Ghosh <30465487+poulamibonnie@users.noreply.github.com>
2026-04-30 11:58:49 -0700

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.cpp
@@ -0,0 +1,49 @@
+#define PII pair<pair<int, int>,int> 
+class Solution {
+    int dr[4] = {1, -1, 0, 0};
+    int dc[4] = {0, 0, 1, -1};
+    bool valid(vector<vector<int>>& grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.size() || c >=grid[0].size() || grid[r][c] == -1) {
+            return false;
+        }
+        return true;
+    }
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        queue<PII>q;
+        set<pair<int,int>> visited;
+
+        //Traverese all chest
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 0) {
+                    q.push({{i,j}, 0});
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            auto temp = q.front();
+            q.pop();
+            int r = temp.first.first;
+            int c = temp.first.second;
+            int path = temp.second;
+            if (visited.count({r,c})) {
+                continue;
+            }
+            grid[r][c] = path;
+            
+            visited.insert({r,c});
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (!valid(grid, nr, nc)) {
+                    continue;
+                }
+                q.push({{nr, nc}, path + 1});
+            }
+        }
+        return;
+    }
+};
