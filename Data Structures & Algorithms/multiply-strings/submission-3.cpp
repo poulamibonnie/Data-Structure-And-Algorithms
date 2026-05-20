446d16460d6f3fc2db527cbef53c177f404cab66
Poulami Das Ghosh <30465487+poulamibonnie@users.noreply.github.com>
2026-05-20 12:58:22 -0700

    Add: multiply-strings - submission-3

---
+++ Data Structures & Algorithms/multiply-strings/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+
+        int n = num1.size(), m = num2.size();
+        vector<int> res(n + m, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                res[i + j] += mul;
+                res[i + j + 1] += res[i + j]/10; //Propagate carry
+                res[i + j] = res[i + j] %10; // Accumulate last digit
+            }
+        }
+
+        reverse(res.begin(), res.end());
+
+        string ans = "";
+        bool lz = true;
+        int i = 0;
+        while ( i < res.size()) {
+            while (lz && res[i] == 0) {
+                i++;
+            }
+            lz = false;
+            ans += to_string(res[i]);
+            i++;
+        }
+        return ans;
+    }
+};
